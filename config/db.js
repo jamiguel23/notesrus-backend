a748778d62957b3c7d9132bde6945ed2e2198fd0
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2022-11-05 15:34:34 -0700

    added config file

---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+  url: "mongodb+srv://jamiguel:g2aeTUoXbMdSruYz@notesrsus.chidqco.mongodb.net/?retryWrites=true&w=majority"
+}
--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient
 const bodyParser = require('body-parser');
-
+const db = require('./config/db.js')
 const app = express();
 const port = 3001
+app.use(bodyParser.urlencoded({extended: true}))
 
 
 
-app.listen(port, () => {
-  console.log('Listening on port', port)
+MongoClient.connect(db.url, (err, database) =>{
+  if (err) return console.log(err);
+
+  require('./app/routes')(app,database)
+  app.listen(port, () => {
+    console.log('Listening on port', port)
+  });
 });
